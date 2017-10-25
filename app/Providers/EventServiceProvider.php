ae22949442a650c81a32a43153bc6e07e75cc927
AlissonRTeles <alisson.teles0@gmail.com>
2017-10-25 19:04:58 -0200

    Primeira Versão

---
+++ app/Providers/EventServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Arvore\Providers;
+
+use Illuminate\Support\Facades\Event;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        'Arvore\Events\Event' => [
+            'Arvore\Listeners\EventListener',
+        ],
+    ];
+
+    /**
+     * Register any events for your application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        //
+    }
+}
---
+++ app/Sociedade.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Arvore;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Sociedade extends Model
+{
+    
+}
